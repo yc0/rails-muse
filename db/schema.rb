4e80becccda31e2af433ddb93a2c6e7a82c33d8d
nelson <nelson.lin@pushme.com.tw>
2015-02-27 18:09:46 +0800

    Added name to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150227094244) do
+ActiveRecord::Schema.define(version: 20150227095548) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20150227094244) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
